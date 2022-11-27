9fb10918fadd4e48978270b4880f0cde5b67aa3c
Yaushiro Taniuchi <tan-y@aosoft.jp>
2022-11-27 18:35:28 +0900

    Fix ffi defines

--- src/bar/ffi.rs
+++ src/bar/ffi.rs
@@ -1,4 +1,4 @@
-use glib::subclass::types::ClassStruct;
+use glib::subclass::types::{ClassStruct, InstanceStructExt, ObjectSubclass};
 
 #[repr(C)]
 pub struct Bar {
@@ -12,4 +12,28 @@ pub struct BarClass {
 
 unsafe impl ClassStruct for BarClass {
     type Type = super::imp::Bar;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bar_get_c(this: *mut Bar) -> i32 {
+    let instance = &*(this as *const <super::imp::Bar as ObjectSubclass>::Instance);
+    instance.imp().c()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bar_set_c(this: *mut Bar, value: i32) {
+    let instance = &*(this as *const <super::imp::Bar as ObjectSubclass>::Instance);
+    instance.imp().set_c(value)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bar_get_d(this: *mut Bar) -> i32 {
+    let instance = &*(this as *const <super::imp::Bar as ObjectSubclass>::Instance);
+    instance.imp().d()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bar_set_d(this: *mut Bar, value: i32) {
+    let instance = &*(this as *const <super::imp::Bar as ObjectSubclass>::Instance);
+    instance.imp().set_d(value)
 }
